a2c46b592dc09d5a1212c4f798418fd7cb861fa0
Fred E <fredilly@article6.org>
2026-09-11 10:36:00 +0800

    chore: remove unused React module from Spike 0

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'wxt';
 
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
   manifest: {
     name: 'VCL',
     description: 'Select visible products in supported video and resolve useful purchase options.',
